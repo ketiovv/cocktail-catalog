0bd4fca3ec01b563d8feea3e5e62d3027969cb7e
Konrad <konrlub566@student.polsl.pl>
2021-01-17 22:14:33 +0100

    Changed theme

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/View/MainActivity.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/View/MainActivity.kt
@@ -1,18 +1,25 @@
 package com.example.cocktailcatalog.View
 
+import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cocktailcatalog.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.P)
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val navController = findNavController(R.id.fragment)
         bottomNavigationView.setupWithNavController(navController)
+        bottomNavigationView.outlineAmbientShadowColor = R.color.white
+
     }
 }
