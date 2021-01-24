0f6fd53fef66ef92ebeab9380344fdf9ac450992
Wojtek <wojtek.kajstura@gmail.com>
2021-01-24 20:54:50 +0100

    added drink entity to db

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Drink.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Drink.kt
@@ -1,33 +1,40 @@
 package com.example.cocktailcatalog.models.entities
 
+import androidx.room.Entity
+import androidx.room.Ignore
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-
+@Entity(
+    tableName = "drink_table"
+)
 data class Drink(
         @Expose
-    @SerializedName("idDrink")
-    var id: String,
-    @SerializedName("strDrink")
-    var name:String,
-    @Expose
-    @SerializedName("strCategory")
-    var category: String,
-    @Expose
-    @SerializedName("strInstructions")
-    var instructions: String,
-    @Expose
-    @SerializedName("strDrinkThumb")
-    var image: String,
-    @Expose
-    @SerializedName("strGlass")
-    var glassType: String,
-    @Expose
-    @SerializedName("strAlcoholic")
-    var alcoholic: String,
-    @Expose(deserialize = false)
-    var favorite: Boolean)  {
-     lateinit var  ingredients : ArrayList<Ingredient>
+        @SerializedName("idDrink")
+        @PrimaryKey
+        var id: String,
+        @SerializedName("strDrink")
+        var name:String,
+        @Expose
+        @SerializedName("strCategory")
+        var category: String,
+        @Expose
+        @SerializedName("strInstructions")
+        var instructions: String,
+        @Expose
+        @SerializedName("strDrinkThumb")
+        var image: String,
+        @Expose
+        @SerializedName("strGlass")
+        var glassType: String,
+        @Expose
+        @SerializedName("strAlcoholic")
+        var alcoholic: String,
+        @Expose(deserialize = false)
+        var favorite: Boolean)  {
+        @Ignore
+        lateinit var  ingredients : ArrayList<Ingredient>
 }
 
 
