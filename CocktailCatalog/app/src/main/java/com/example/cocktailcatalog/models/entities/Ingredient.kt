422a7bcc2cc549324e8c848d54334d598a00281b
Wojtek <wojtek.kajstura@gmail.com>
2021-01-24 20:55:00 +0100

    added ingredient entity to db

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Ingredient.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Ingredient.kt
@@ -1,11 +1,17 @@
 package com.example.cocktailcatalog.models.entities
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
+@Entity(
+        tableName = "ingredient_table"
+)
 data class Ingredient(
         @Expose
         @SerializedName("idIngredient")
+        @PrimaryKey
         var id: Int,
         @Expose
         @SerializedName("strIngredient")
