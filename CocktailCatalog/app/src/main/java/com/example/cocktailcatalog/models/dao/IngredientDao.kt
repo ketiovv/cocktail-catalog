50cef79cb25bcc1a4eba9023a1351457d78d0f0f
Wojtek <wojtek.kajstura@gmail.com>
2021-01-24 21:03:04 +0100

    added ingredient dao

---
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/dao/IngredientDao.kt
@@ -0,0 +1,11 @@
+package com.example.cocktailcatalog.models.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import com.example.cocktailcatalog.models.entities.Ingredient
+
+@Dao
+interface IngredientDao {
+    @Insert
+    suspend fun insert(ingredient: Ingredient)
+}
