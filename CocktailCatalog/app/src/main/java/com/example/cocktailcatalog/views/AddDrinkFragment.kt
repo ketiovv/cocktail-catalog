978231bf989a16812284e1db7e52d8e2a64beda1
Wojtek <wojtek.kajstura@gmail.com>
2021-01-27 16:36:28 +0100

    implemented adding drink to db by button

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkFragment.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.cocktailcatalog.R
+import com.example.cocktailcatalog.viewmodels.DrinkViewModel
+import kotlinx.android.synthetic.main.fragment_add_drink.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +24,7 @@ class AddDrinkFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var drinkViewModel:DrinkViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,8 +38,18 @@ class AddDrinkFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        drinkViewModel = ViewModelProvider(requireActivity()).get(DrinkViewModel::class.java)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_drink, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonAddDrinkToLocalDatabase.setOnClickListener {
+            drinkViewModel.addDrinkToLocalDatabase("test","test", "test", "test", "test", "test")
+        }
     }
 
     companion object {
