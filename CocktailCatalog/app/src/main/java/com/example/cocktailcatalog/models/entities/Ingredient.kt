5ee0026eebd04ddc810120bfd6693c699c4ca89f
Wojtek <wojtek.kajstura@gmail.com>
2021-01-27 18:25:53 +0100

    set ingredient primary key to auto generating

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Ingredient.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Ingredient.kt
@@ -11,7 +11,7 @@ import com.google.gson.annotations.SerializedName
 data class Ingredient(
         @Expose
         @SerializedName("idIngredient")
-        @PrimaryKey
+        @PrimaryKey(autoGenerate = true)
         var id: Int,
         @Expose
         @SerializedName("strIngredient")
