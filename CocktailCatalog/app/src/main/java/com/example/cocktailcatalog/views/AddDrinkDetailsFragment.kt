ef895be0a1e9c5f0d9f670379e6e989fe13492e7
Wojtek <wojtek.kajstura@gmail.com>
2021-01-28 16:42:55 +0100

    implemented getting first page data as argument

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkDetailsFragment.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkDetailsFragment.kt
@@ -1,32 +1,23 @@
 package com.example.cocktailcatalog.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.cocktailcatalog.R
+import com.example.cocktailcatalog.models.entities.LocalDrinkNameAndIngredients
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+private const val ARG_FIRST_PAGE_DATA = "firstPageData"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AddDrinkDetailsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddDrinkDetailsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var firstPageData :LocalDrinkNameAndIngredients
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            firstPageData = it.getParcelable(ARG_FIRST_PAGE_DATA)!!
         }
     }
 
@@ -36,22 +27,22 @@ class AddDrinkDetailsFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_add_drink_details, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        Log.d("test",firstPageData.name)
+        Log.d("test","ingredient list: ")
+        for (x in firstPageData.ingredients){
+            Log.d("test", x.name)
+        }
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddDrinkDetailsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(firstPageData: LocalDrinkNameAndIngredients) =
                 AddDrinkDetailsFragment().apply {
                     arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
+                        putParcelable(ARG_FIRST_PAGE_DATA, firstPageData)
                     }
                 }
     }
