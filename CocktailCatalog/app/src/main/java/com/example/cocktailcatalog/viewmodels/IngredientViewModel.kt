3bd2294ffe8f02ee16cc60bcdfd31ffd7bb6df58
Wojtek <wojtek.kajstura@gmail.com>
2021-01-28 18:31:54 +0100

    implemented adding ingredient to db in ingredient vm

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/viewmodels/IngredientViewModel.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/viewmodels/IngredientViewModel.kt
@@ -1,21 +1,39 @@
 package com.example.cocktailcatalog.viewmodels
 
+import android.app.Application
 import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.cocktailcatalog.api.Deserializers.IngredientsNamesListDeserializer
 
 import com.example.cocktailcatalog.api.ApiRoutes
 import com.example.cocktailcatalog.api.IApiRequest
+import com.example.cocktailcatalog.models.AppDatabase
+import com.example.cocktailcatalog.models.entities.Ingredient
 import com.example.cocktailcatalog.models.entities.IngredientNamesList
+import com.example.cocktailcatalog.models.repositories.IngredientRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.Retrofit
 import retrofit2.awaitResponse
 
-class IngredientViewModel : ViewModel() {
+class IngredientViewModel(application: Application) : AndroidViewModel(application) {
     var listOfIngredientNames = MutableLiveData<IngredientNamesList>()
+
+    private val ingredientRepository = IngredientRepository(AppDatabase.getDatabase(application).ingredientDao())
+
+    // DB METHODS
+    fun addIngredientToLocalDatabase(name: String, measure: String){
+        val ingredient = Ingredient(0, name, measure)
+        viewModelScope.launch {
+            ingredientRepository.add(ingredient)
+        }
+    }
+
+    // API METHODS
 
     fun getIngredientNameList(){
         GlobalScope.launch(Dispatchers.IO) {
