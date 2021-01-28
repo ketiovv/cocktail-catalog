fee1a4f7911fadc2ed7d485a7b622efc42579709
Wojtek <wojtek.kajstura@gmail.com>
2021-01-28 16:24:32 +0100

    implement parcelable in ingredient

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Ingredient.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/entities/Ingredient.kt
@@ -1,5 +1,7 @@
 package com.example.cocktailcatalog.models.entities
 
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.gson.annotations.Expose
@@ -18,7 +20,33 @@ data class Ingredient(
         var name: String,
         @Expose(serialize = false, deserialize = false)
         var measure: String
-) {
+):Parcelable {
+        constructor(parcel: Parcel) : this(
+                parcel.readInt(),
+                parcel.readString()!!,
+                parcel.readString()!!
+        ) {
+        }
+
+        override fun writeToParcel(parcel: Parcel, flags: Int) {
+                parcel.writeInt(id)
+                parcel.writeString(name)
+                parcel.writeString(measure)
+        }
+
+        override fun describeContents(): Int {
+                return 0
+        }
+
+        companion object CREATOR : Parcelable.Creator<Ingredient> {
+                override fun createFromParcel(parcel: Parcel): Ingredient {
+                        return Ingredient(parcel)
+                }
+
+                override fun newArray(size: Int): Array<Ingredient?> {
+                        return arrayOfNulls(size)
+                }
+        }
 }
 
 
