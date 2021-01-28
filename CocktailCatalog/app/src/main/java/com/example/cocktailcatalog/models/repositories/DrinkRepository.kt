be06018c697a3d57fdd1c4c2ca3cb4e604733b1c
Wojtek <wojtek.kajstura@gmail.com>
2021-01-28 20:58:53 +0100

    created all drinks property in drink repo

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/repositories/DrinkRepository.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/repositories/DrinkRepository.kt
@@ -1,9 +1,11 @@
 package com.example.cocktailcatalog.models.repositories
 
+import androidx.lifecycle.LiveData
 import com.example.cocktailcatalog.models.dao.DrinkDao
 import com.example.cocktailcatalog.models.entities.Drink
 import com.example.cocktailcatalog.models.entities.LocalDrink
 
 class DrinkRepository(private val drinkDao: DrinkDao) {
+    val allDrinks: LiveData<List<LocalDrink>> = drinkDao.getAll()
     suspend fun add(drink: LocalDrink): Long = drinkDao.insert(drink)
 }
