27dc8e35ce358af3df963d2f930dcf857006b496
Wojtek <wojtek.kajstura@gmail.com>
2021-01-28 18:24:09 +0100

    implemented measure adapter (problem with measure unit spinner listener)

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/adapters/IngredientMeasureAdapter.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/adapters/IngredientMeasureAdapter.kt
@@ -3,11 +3,14 @@ package com.example.cocktailcatalog.adapters
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cocktailcatalog.R
 import com.example.cocktailcatalog.models.entities.Ingredient
-import com.google.android.material.transition.Hold
 
 class IngredientMeasureAdapter(var ingredients: ArrayList<Ingredient>):RecyclerView.Adapter<IngredientMeasureAdapter.IngredientHolder>() {
     inner class IngredientHolder(view: View): RecyclerView.ViewHolder(view)
@@ -21,6 +24,36 @@ class IngredientMeasureAdapter(var ingredients: ArrayList<Ingredient>):RecyclerV
     override fun onBindViewHolder(holder: IngredientHolder, position: Int) {
         val ingredientName = holder.itemView.findViewById<TextView>(R.id.textViewIngredientName)
         ingredientName.text = ingredients[position].name
+
+        val spinner = holder.itemView.findViewById<Spinner>(R.id.spinnerUnit)
+        val measureValue = holder.itemView.findViewById<TextView>(R.id.editTextNewDrinkIngredientMeasure)
+
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                holder.itemView.context,
+                android.R.layout.simple_spinner_item, arrayOf("ml", "xd")
+            )
+            spinner.adapter = adapter
+        }
+        // TODO: NOT WORK XD
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                ingredients[position].measure = measureValue.text.toString() + " " + spinner.selectedItem
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                ingredients[position].measure = measureValue.text.toString() + " " + spinner.selectedItem
+            }
+        }
+
+        measureValue.addTextChangedListener(){
+            ingredients[position].measure = measureValue.text.toString() + " " + spinner.selectedItem
+        }
     }
 
     override fun getItemCount() = ingredients.size
--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkDetailsFragment.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkDetailsFragment.kt
@@ -6,12 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cocktailcatalog.R
+import com.example.cocktailcatalog.adapters.IngredientMeasureAdapter
 import com.example.cocktailcatalog.models.entities.LocalDrinkNameAndIngredients
 import com.example.cocktailcatalog.viewmodels.DrinkViewModel
 import kotlinx.android.synthetic.main.fragment_add_drink_details.*
+import kotlinx.android.synthetic.main.fragment_search_drink.*
 
 private const val ARG_FIRST_PAGE_DATA = "firstPageData"
 
@@ -20,6 +25,8 @@ class AddDrinkDetailsFragment : Fragment() {
 
     private lateinit var drinkViewModel:DrinkViewModel
 
+    private lateinit var ingredientMeasureAdapter: IngredientMeasureAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,8 +37,10 @@ class AddDrinkDetailsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        drinkViewModel = ViewModelProvider(requireActivity()).get(DrinkViewModel::class.java)
 
+        drinkViewModel = ViewModelProvider(requireActivity()).get(DrinkViewModel::class.java)
+        ingredientMeasureAdapter = IngredientMeasureAdapter(firstPageData.ingredients)
+        viewManager = LinearLayoutManager(requireContext())
 
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_add_drink_details, container, false)
@@ -40,11 +49,9 @@ class AddDrinkDetailsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // testowo..
-        Log.d("test",firstPageData.name)
-        Log.d("test","ingredient list: ")
-        for (x in firstPageData.ingredients){
-            Log.d("test", x.name)
+        recyclerViewIngredientsMeasureEntering.apply{
+            adapter = ingredientMeasureAdapter
+            layoutManager = viewManager
         }
 
         val instructions = editTextNewDrinkInstructions.text
@@ -53,7 +60,7 @@ class AddDrinkDetailsFragment : Fragment() {
             if (instructions.isNullOrBlank()){
                 editTextNewDrinkInstructions.error = "Instrutions can't be empty!"
             }// TODO: walidacja na miary
-            else{
+            else {
                 // add drink to database
                 drinkViewModel.addDrinkToLocalDatabase(
                     firstPageData.name,
@@ -61,18 +68,25 @@ class AddDrinkDetailsFragment : Fragment() {
                     instructions.toString(),
                     "test",
                     "test",
-                    "test")
+                    "test"
+                )
 
                 // TODO: add ingredients to db
 
                 // TODO: add relations to db
 
                 // TODO: later maybe it should navigate to view of this drink
-                view.findNavController().navigate(R.id.action_addDrinkDetailsFragment_to_addDrinkFragment)
+                view.findNavController()
+                    .navigate(R.id.action_addDrinkDetailsFragment_to_addDrinkFragment)
+
+                // testowo..
+                Log.d("test", firstPageData.name)
+                Log.d("test", "ingredient list from adapter: ")
+                for (x in ingredientMeasureAdapter.ingredients) {
+                    Log.d("test", x.name + ", measure: " + x.measure)
+                }
             }
         }
-
-
     }
 
     companion object {
