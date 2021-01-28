1b6097c64b3c0b57dcb1e7c9b3b946c025128a42
Wojtek <wojtek.kajstura@gmail.com>
2021-01-28 20:57:36 +0100

    created get all query in local drink dao

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/dao/DrinkDao.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/models/dao/DrinkDao.kt
@@ -1,12 +1,17 @@
 package com.example.cocktailcatalog.models.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.example.cocktailcatalog.models.entities.Drink
 import com.example.cocktailcatalog.models.entities.LocalDrink
 
 @Dao
 interface DrinkDao {
+    @Query("SELECT * FROM DRINK_TABLE")
+    fun getAll(): LiveData<List<LocalDrink>>
+
     @Insert
     suspend fun insert(drink: LocalDrink):Long
 }
