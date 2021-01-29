07950f992c0d50190ab27c9c4369695a21ec7d4f
Wojtek <wojtek.kajstura@gmail.com>
2021-01-29 20:17:24 +0100

    implemented displaying ingredients of drink

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/LocalDrinkDetailsFragment.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/LocalDrinkDetailsFragment.kt
@@ -1,15 +1,18 @@
 package com.example.cocktailcatalog.views
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.cocktailcatalog.R
+import com.example.cocktailcatalog.models.entities.Ingredient
 import com.example.cocktailcatalog.viewmodels.DrinkViewModel
+import com.example.cocktailcatalog.viewmodels.IngredientViewModel
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_drink_details.*
-import kotlinx.android.synthetic.main.fragment_drink_details.textViewDrinkNameD
-import kotlinx.android.synthetic.main.fragment_drink_details.textViewInstructions
 import kotlinx.android.synthetic.main.fragment_local_drink_details.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -27,6 +30,8 @@ class LocalDrinkDetailsFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private lateinit var ingredientViewModel: IngredientViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -37,8 +42,21 @@ class LocalDrinkDetailsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        ingredientViewModel = ViewModelProvider(requireActivity()).get(IngredientViewModel::class.java)
+
+        ingredientViewModel.drinkIngredients.observe(viewLifecycleOwner){
+            updateIngredients(it)
+        }
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_local_drink_details, container, false)
+    }
+
+    private fun updateIngredients(list: List<Ingredient>) {
+        textViewIngredientsListF.text = ""
+        for (ingredient in list){
+            Log.d("test", ingredient.name)
+            textViewIngredientsListF.text = textViewIngredientsListF.text.toString() + "${ingredient.measure} ${ingredient.name} \n"
+        }
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -46,6 +64,18 @@ class LocalDrinkDetailsFragment : Fragment() {
 
         textViewDrinkNameDF.text = DrinkViewModel.selectedLocalDrink.name
         textViewInstructionsF.text = DrinkViewModel.selectedLocalDrink.instructions
+
+        if (DrinkViewModel.selectedLocalDrink.image.isNullOrBlank()){
+            imageViewF.visibility  = View.GONE
+        }
+        else{
+            Picasso.get().load(DrinkViewModel.selectedLocalDrink.image).resize(700, 700)
+                .centerCrop()
+                .into(imageViewF)
+        }
+        //TODO: dam musisz dodać do tego picassa żeby jak nie ma linku to imageview.visibility  = View.gone
+
+
     }
 
     companion object {
