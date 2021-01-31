842d56320253b3ef9d64f0cb3fbba32ebd9e4fd7
Karol Nawrot <physalium2@gmail.com>
2021-01-31 22:50:02 +0100

    Navigation and shared preferences integration

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AgeCheckFragment.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AgeCheckFragment.kt
@@ -1,18 +1,45 @@
 package com.example.cocktailcatalog.views
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.cocktailcatalog.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_age_check.*
 
 
 class AgeCheckFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    private lateinit var sharedPref: SharedPreferences
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
         return inflater.inflate(R.layout.fragment_age_check, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        button_yes.setOnClickListener {
+
+
+            sharedPref.edit().putString(getString(R.string.over_18), "true").commit()
+            activity?.runOnUiThread {
+                activity?.bottomNavigationView!!.visibility = View.VISIBLE
+            }
+            it.findNavController().navigate(R.id.searchFragment)
+        }
+        button_no.setOnClickListener {
+            activity?.finishAndRemoveTask()
+        }
+
     }
 
 
--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/MainActivity.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/MainActivity.kt
@@ -1,31 +1,43 @@
 package com.example.cocktailcatalog.views
 
 import android.annotation.SuppressLint
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
-import com.example.cocktailcatalog.viewmodels.DrinkViewModel
-
-import androidx.annotation.RequiresApi
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.cocktailcatalog.R
+import com.example.cocktailcatalog.viewmodels.DrinkViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var viewModel: DrinkViewModel
+    private lateinit var sharedPref: SharedPreferences
 
-    @RequiresApi(Build.VERSION_CODES.P)
+    //@RequiresApi(Build.VERSION_CODES.P)
     @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
         val navController = findNavController(R.id.fragment)
         bottomNavigationView.setupWithNavController(navController)
+
+        sharedPref = this.getPreferences(Context.MODE_PRIVATE)
+
+
+        if (!sharedPref.contains(getString(R.string.over_18))) {
+            bottomNavigationView.visibility = View.GONE
+            navController.navigate(R.id.ageCheckFragment)
+        }
+
+
         //bottomNavigationView.outlineAmbientShadowColor = R.color.white
 
         viewModel = ViewModelProvider(this).get(DrinkViewModel::class.java)
