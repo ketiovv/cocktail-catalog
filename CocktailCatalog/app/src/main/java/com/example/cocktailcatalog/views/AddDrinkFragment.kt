753a939d7ed16a1bb48e40eed906373456678384
Konrad <konrlub566@student.polsl.pl>
2021-01-31 19:29:17 +0100

    Cleared files, ui tweaks

--- CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkFragment.kt
+++ CocktailCatalog/app/src/main/java/com/example/cocktailcatalog/views/AddDrinkFragment.kt
@@ -22,33 +22,12 @@ import com.example.cocktailcatalog.viewmodels.DrinkViewModel
 import com.example.cocktailcatalog.viewmodels.IngredientViewModel
 import kotlinx.android.synthetic.main.fragment_add_drink.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AddDrinkFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AddDrinkFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var ingredientViewModel: IngredientViewModel
 
     private lateinit var ingredientListAdapter: IngredientListAdapter
     private lateinit var viewManager: RecyclerView.LayoutManager
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -80,8 +59,6 @@ class AddDrinkFragment : Fragment() {
             layoutManager = viewManager
         }
 
-
-
         val name = editTextNewDrinkDescription.text;
         buttonMoveToNextPage.setOnClickListener {
             if (name.toString().isNullOrBlank()){
@@ -105,22 +82,9 @@ class AddDrinkFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AddDrinkFragment.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AddDrinkFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() = AddDrinkFragment()
+
     }
 }
